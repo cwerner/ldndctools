9333280c34c97daef66a08018f8af4f85fd9615d
Christian Werner <chr.werner@me.com>
2018-08-17 15:12:56 +0200

    Add db split script

---
+++ split4db.py
@@ -0,0 +1,38 @@
+import string
+import gzip
+import sys
+
+# read the NLCC 1.0 file(s)
+
+
+climate_dbfile = sys.argv[1]
+
+climate_lines = gzip.open( climate_dbfile, 'rb' ).readlines()
+
+fileOpen = False
+
+global_header = None
+
+for lcnt, cl in enumerate(climate_lines):
+
+    if "climate" in cl:
+        if fileOpen:
+            f.close()
+        else:
+            # should only occure once
+            global_header = "".join(climate_lines[0:lcnt])
+
+        # sneek-peek at next line to get filename
+
+        next_line = climate_lines[lcnt+1]
+
+        if "id" in next_line:
+            theId = int(next_line.split('=')[-1])
+            f = gzip.open("db/climate_%08d.txt.gz" % theId, 'w')
+            f.write(global_header)
+            fileOpen = True
+    if fileOpen:
+        f.write(cl)
+
+
+
