4d067e73a095ca784e5f0c04c47febdade767453
Christian Werner <chr.werner@me.com>
2021-04-23 15:11:40 +0200

    Reenable file coordinate selection

--- ldndctools/misc/create_data.py
+++ ldndctools/misc/create_data.py
@@ -1,18 +1,28 @@
+from typing import Union
+
 import rioxarray  # noqa
 import xarray as xr
 
-from ldndctools.cli.selector import Selector
+from ldndctools.cli.selector import CoordinateSelection, Selector
 from ldndctools.io.xmlwriter import SiteXmlWriter
 from ldndctools.misc.types import RES
 
 
 def create_dataset(
-    soil: xr.Dataset, selector: Selector, res: RES, progressbar, status_widget=None
+    soil: xr.Dataset,
+    selector: Union[Selector, CoordinateSelection],
+    res: RES,
+    progressbar,
+    status_widget=None,
 ):
     soil = soil.load()
-    soil = soil.rio.write_crs("epsg:4326")
-    soil = soil.rio.clip(selector.gdf_mask.geometry, selector.gdf_mask.crs)
-    soil = soil.where(soil.PROP1.sel(lev=1) > 0)
+    soil = soil.rio.write_crs(4326)
+
+    if isinstance(selector, Selector):
+        soil = soil.rio.clip(selector.gdf_mask.geometry)
+        soil = soil.where(soil.PROP1.sel(lev=1) > 0)
+    else:
+        soil = soil.sel(lat=selector.lats, lon=selector.lons, method="nearest")
 
     xmlwriter = SiteXmlWriter(soil, res=res)
     site_xml = xmlwriter.write(progressbar=progressbar, status_widget=status_widget)
