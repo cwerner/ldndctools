236866dff709449d1234f58c7c34d36e3af0fc26
Christian Werner <chr.werner@me.com>
2021-04-26 11:55:53 +0200

    Add file-driven dlsc option
    Former-commit-id: 5e77a328bdfc9e57448873963cd5ef732bd8c5ee

--- ldndctools/misc/create_data.py
+++ ldndctools/misc/create_data.py
@@ -1,5 +1,6 @@
 from typing import Union
 
+import numpy as np
 import rioxarray  # noqa
 import xarray as xr
 
@@ -21,11 +22,31 @@ def create_dataset(
     if isinstance(selector, Selector):
         soil = soil.rio.clip(selector.gdf_mask.geometry)
         soil = soil.where(soil.PROP1.sel(lev=1) > 0)
-    else:
-        soil = soil.sel(lat=selector.lats, lon=selector.lons, method="nearest")
+        xmlwriter = SiteXmlWriter(soil, res=res)
+        site_xml = xmlwriter.write(progressbar=progressbar, status_widget=status_widget)
 
-    xmlwriter = SiteXmlWriter(soil, res=res)
-    site_xml = xmlwriter.write(progressbar=progressbar, status_widget=status_widget)
+    else:
+        # TODO: this should be possible to achieve in a cleaner way!
+        soil_orig = soil.sel(lat=selector.lats, lon=selector.lons, method="nearest")
+        selected_lats = xr.DataArray(selector.lats, dims="points")
+        selected_lons = xr.DataArray(selector.lons, dims="points")
+        soil_pointwise = soil_orig.sel(
+            lat=selected_lats, lon=selected_lons, method="nearest"
+        )
+
+        soil_new = xr.ones_like(soil_orig) * np.nan
+        for p in soil_pointwise.points:
+            s = soil_pointwise.sel(points=p)
+            for v in soil_pointwise.data_vars:
+                soil_new[v].loc[dict(lat=s.lat, lon=s.lon)] = s[v]
+
+        xmlwriter = SiteXmlWriter(soil_new, res=res)
+        site_xml = xmlwriter.write(
+            progressbar=progressbar,
+            status_widget=status_widget,
+            coords=zip(selector.lons, selector.lats),
+        )
+
     site_nc = xmlwriter.arrays
 
     return site_xml, site_nc
