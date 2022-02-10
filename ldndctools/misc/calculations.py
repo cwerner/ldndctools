de7db73bdbb8dbb4ee4bf651f97961762131b09c
Christian Werner <chr.werner@me.com>
2022-02-10 16:46:10 +0100

    Cleanup calc_litter_properties - this func is currently not used

--- ldndctools/misc/calculations.py
+++ ldndctools/misc/calculations.py
@@ -1,48 +1,39 @@
 import math
-import xml.dom.minidom as MD
-import xml.etree.cElementTree as ET
+from typing import Tuple
 
 from ldndctools.misc.errors import ParameterMissingError
 from ldndctools.misc.types import LayerData
 
 
-def prettify(elem):
-    """Return a pretty-printed XML string for the Element."""
-    rough_string = ET.tostring(elem, "utf-8")
-    reparsed = MD.parseString(rough_string)
-    str1 = reparsed.toprettyxml(indent="  ")
-    str2 = []
-    ss = str1.split("\n")
-    for s in ss:
-        x = "".join(s.split())
-        if x != "":
-            str2.append(s)
-    return "\n".join(str2) + "\n"
-
-
-def calcLitter(litterMass, litname):  # mass in t C ha-1
-    if litname == "MULL":
+# currently not used
+def calc_litter_properties(
+    litter_mass: float, litter_name: str
+) -> Tuple[float, float, float]:  # mass in t C ha-1
+    """calculate litter height based on litter type and mass"""
+    if litter_name == "MULL":
         density = 0.2
-        accumulationFactor = 1.5
-    elif litname == "MODER":
+        accumulation_factor = 1.5
+    elif litter_name == "MODER":
         density = 0.25
-        accumulationFactor = 2.5
+        accumulation_factor = 2.5
     else:
         density = 0.3
-        accumulationFactor = 3.5
+        accumulation_factor = 3.5
 
     # explanation:
     #   (tCha-1) > x2 > tBMha-1 > x0.1 > kgBMm-2 > x0.1 > gBMcm-2 >
     #   /density > height_cm > *10 > height_mm
     # littermass (t C ha-1) * 2 (BM conv) * 0.1 * 0.1 / density * 10
-    depth = ((litterMass * accumulationFactor * 2 * 0.1 * 0.1) / density) * 10.0
-    numberOfLayers = math.floor(depth / 20.0)
-    layerHeight = depth
-    if numberOfLayers != 0:
-        layerHeight = 20.0 + ((depth % 20.0) / numberOfLayers)
-    return (density, depth, layerHeight)
+    depth = ((litter_mass * accumulation_factor * 2 * 0.1 * 0.1) / density) * 10.0
+    n_layers = math.floor(depth / 20.0)
+    layer_height = depth
+    if n_layers > 0:
+        layer_height = 20.0 + ((depth % 20.0) / n_layers)
+    return (density, depth, layer_height)
 
 
+# currently not used
+# TODO: remove?
 def calcHeight(TK, N):
     if TK == -9999:
         TKmm = -9999
