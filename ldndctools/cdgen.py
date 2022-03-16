ee6cccefc4e09b5f3ee4dc5d1c9e656c9b4493ea
Christian Werner <christian.werner@kit.edu>
2022-03-16 14:20:20 +0100

    Big rewrite of climate processing. Also add a CLI

--- ldndctools/cdgen.py
+++ ldndctools/cdgen.py
@@ -2,7 +2,9 @@ import gzip
 import io
 from dataclasses import asdict, dataclass
 from importlib import resources
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Iterable, Union, Optional
+
+import platform
 
 import dask
 import intake
@@ -12,11 +14,16 @@ import urllib3
 import xarray as xr
 from dask.distributed import Client
 
-from ldndctools.misc.geohash import coords2geohash_dec
+import argparse
+from pathlib  import Path
+
+from ldndctools.misc.geohash import coords2geohash_dec, geohash_dec2coords 
 from ldndctools.misc.types import BoundingBox
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+import warnings
+warnings.filterwarnings('ignore')
 
 @dataclass
 class ClimateSiteStats:
@@ -35,19 +42,21 @@ class ClimateSiteStats:
 
 
 def subset_climate_data(
-    *, bbox: BoundingBox, time_min: Optional[str] = None, time_max: Optional[str] = None
+    *, bbox: Optional[BoundingBox] = None, date_min: Optional[str] = None, date_max: Optional[str] = None
 ) -> xr.Dataset:
 
     with resources.path("data", "catalog.yml") as cat:
         catalog = intake.open_catalog(str(cat))
         ds = catalog["climate_era5land_hr"].to_dask()
-        ds = ds.sel(lat=slice(bbox.y1, bbox.y2), lon=slice(bbox.x1, bbox.x2))
 
-        if time_min:
-            ds = ds.sel(time=slice(time_min, None))
+        if bbox:
+            ds = ds.sel(lat=slice(bbox.y1, bbox.y2), lon=slice(bbox.x1, bbox.x2))
 
-        if time_max:
-            ds = ds.sel(time=slice(None, time_max))
+        if date_min:
+            ds = ds.sel(time=slice(date_min, None))
+
+        if date_max:
+            ds = ds.sel(time=slice(None, date_max))
 
     return ds
 
@@ -56,7 +65,7 @@ def amplitude(da):
     return (da.max(dim="time") - da.min(dim="time")) / 2
 
 
-def fill_header_global(time):
+def fill_header_global(time: pd.Timestamp) -> str:
     txt = f"""
 %global
 time     = "{time}/1"
@@ -66,74 +75,48 @@ time     = "{time}/1"
     return txt
 
 
-def fill_header(data: ClimateSiteStats):
+def fill_header(d: ClimateSiteStats) -> str:
 
-    txt = """
+    txt = f'''
 %climate
-name = "climate_{id}.txt.gz"
+name = "climate_{d.id}.txt.gz"
 archive = "ERA5 Land (resampled to LDNDC HR)"
-id   = "{id}"
+id   = "{d.id}"
 
 %attributes
-elevation  = {elevation}
-latitude   = {latitude:.6f}
-longitude  = {longitude:.6f}
+elevation  = {d.elevation}
+latitude   = {d.latitude:.6f}
+longitude  = {d.longitude:.6f}
 
-wind speed = {wind_speed:.2f}
-cloudiness = {cloudiness:.1f}
-rainfall intensity = {rainfall_intensity}
+wind speed = {d.wind_speed:.2f}
+cloudiness = {d.cloudiness:.1f}
+rainfall intensity = {d.rainfall_intensity}
 
-annual precipitation  = {annual_precipitation:.1f}
-temperature average   = {temperature_average:.2f}
-temperature amplitude = {temperature_amplitude:.2f}
+annual precipitation  = {d.annual_precipitation:.1f}
+temperature average   = {d.temperature_average:.2f}
+temperature amplitude = {d.temperature_amplitude:.2f}
 
 %data
 *       *       tavg    tmin    tmax    grad    prec    rh      wind
-""".format(
-        **asdict(data)
-    )
+'''
     return txt
 
 
-def writer(df: pd.DataFrame, pid: int, *, lookup: Dict[str, Any]):
-    buffer = io.StringIO()
-    print(">>>>>>>>>>>>>>")
-    print(df.head(2))
-    print(pid)
-    print(len(list(lookup.keys())))
-    print(">>>>>>>>>>>>>>")
+def writer(df: pd.DataFrame, pid: int, *, lookup: Dict[int, ClimateSiteStats], args: Any = None) -> Iterable[int]:
 
-    header_global = fill_header_global(df.time.min())
+    buffer = io.StringIO()
+    
+    header_global = fill_header_global(df.time.min().date())
     buffer.write(header_global)
 
-    dummy_data = ClimateSiteStats(
-        id=942889503,
-        latitude=30,
-        longitude=30,
-        wind_speed=10,
-        annual_precipitation=1000,
-        temperature_average=20,
-        temperature_amplitude=10,
-    )
-
+    all_hashes: Iterable[int] = []
     for geohash, gdf in df.groupby(df.index):
-        data = lookup.get(geohash, dummy_data)
-        # try:
-        #     data = lookup[geohash]
-        # except KeyError:
-        #     continue
-
-        # if data is None:
-        #     continue
-
-        # if hasattr(data, 'id') is False:
-        #     continue
-
+        geohash = int(geohash)
+        data = lookup[geohash]
         header = fill_header(data)
         buffer.write(header)
 
         gdf = gdf.sort_values(by="time")
-
         cols = ["year", "jday", "tavg", "tmin", "tmax", "grad", "prec", "rh", "wind"]
         values = [
             gdf.time.dt.year,
@@ -150,48 +133,81 @@ def writer(df: pd.DataFrame, pid: int, *, lookup: Dict[str, Any]):
         fmt = "%-8.0f%-8.0f%-8.2f%-8.2f%-8.2f%-8.2f%-8.1f%-8.1f%-8.2f"
         np.savetxt(buffer, pd.DataFrame(dict(zip(cols, values))), fmt=fmt)
 
+        all_hashes.append(geohash)
+
         buffer.write("\n\n")
     buffer.seek(0)
 
-    with gzip.open(f"testdata/climdata-{pid:03}.txt.gz", "wt") as f:
+    with gzip.open(args.outfolder / f"climdata-{pid:03}.txt.gz", "wt") as f:
         f.write(buffer.read())
 
-    return None
-
-
-# def inner(mask, lat, lon):
-#     return -1 if np.isnan(mask) else coords2geohash_dec(lat=lat, lon=lon)
+    return all_hashes
 
 
 @np.vectorize
-def inner(x, lat, lon):
+def inner_func(x, lat, lon):
     return -1 if np.isnan(x) else coords2geohash_dec(lat=lat, lon=lon)
 
 
-def geohash_xr(mask):  # , lat, lon):
-    data = xr.ones_like(mask, dtype=np.int64) * -1
-
-    lats = np.tile(mask.lat.values, len(mask.lon.values)).reshape(mask.values.shape).T
-    lons = np.tile(mask.lon.values, len(mask.lat.values)).reshape(mask.values.shape)
-    data[:] = inner(mask, lats, lons)
+def geohash_xr(mask: xr.DataArray) -> xr.DataArray:
+    lon_xr = mask.lon.broadcast_like(mask)
+    lat_xr = mask.lat.broadcast_like(mask)
+    data = xr.apply_ufunc(
+        inner_func,
+        mask, 
+        lat_xr,
+        lon_xr,
+        output_dtypes=[np.int64]
+        )
+    assert data.dtype == np.int64
     return data
 
 
-def main():
-    ds_vn = subset_climate_data(
-        # bbox=BoundingBox(x1=101.5, x2=109.5, y1=8.0, y2=23.5),
-        bbox=BoundingBox(x1=104.5, x2=105.5, y1=9.0, y2=10.0),
-        # time_min='2000-01-01',
-        # time_max='2002-12-31'
+
+def get_boundingbox(bbox_flag:str) -> Union[None, BoundingBox]:
+    if not bbox_flag:
+        return None
+
+    x1, y1, x2, y2 = [float(x) for x in bbox_flag.split(",")]
+    try:
+        bbox = BoundingBox(x1=x1, y1=y1, x2=x2, y2=y2)
+    except ValidationError:
+        print("Ilegal bounding box coordinates specified.")
+        print("required: x1,y1,x2,y2 [cond: x1<x2, y1<y2]")
+        print(f"given:    x1={x1},y1={y1},x2={x2},y2={y2}")
+        exit(1)
+    return bbox
+
+
+def get_mask(mask_flag:str) -> xr.DataArray:
+    if not mask_flag:
+        return None
+    
+    filepath, var = mask_flag.split(":")
+    da = xr.open_dataset(filepath)[var].load()
+    return da.where(da>0)
+
+
+def main(args):
+
+    bbox = get_boundingbox(args.bbox)
+    mask = get_mask(args.mask)
+
+    # mask = xr.open_dataset("VN_MISC5_V2.nc")["rice_rot"]
+    # mask = xr.where(mask > 0, 1, np.nan)
+
+
+    ds = subset_climate_data(
+        bbox=bbox, # BoundingBox(x1=101.5, x2=109.5, y1=8.0, y2=23.5),
+        #bbox=BoundingBox(x1=104.5, x2=105.5, y1=9.0, y2=10.0),
+        date_min=args.date_min,
+        date_max=args.date_max,
     )
 
-    mask = xr.open_dataset("VN_MISC5_V2.nc")["rice_rot"]
-    mask = xr.where(mask > 0, 1, np.nan).values
-
-    tavg_year = ds_vn.tavg.groupby("time.year").mean(dim="time").mean(dim="year")
-    tamp_year = ds_vn.tavg.groupby("time.year").apply(amplitude).mean(dim="year")
-    prec_year = ds_vn.prec.groupby("time.year").sum(dim="time").mean(dim="year")
-    wind_year = ds_vn.wind.groupby("time.year").mean(dim="time").mean(dim="year")
+    tavg_year = ds.tavg.groupby("time.year").mean(dim="time").mean(dim="year")
+    tamp_year = ds.tavg.groupby("time.year").apply(amplitude).mean(dim="year")
+    prec_year = ds.prec.groupby("time.year").sum(dim="time").mean(dim="year")
+    wind_year = ds.wind.groupby("time.year").mean(dim="time").mean(dim="year")
 
     stats = xr.Dataset()
     stats["tavg"] = tavg_year
@@ -199,17 +215,17 @@ def main():
     stats["prec"] = prec_year
     stats["wind"] = wind_year
 
-    # stats["mask"] = xr.ones_like(stats.tavg)
-    # stats["mask"][:] = mask
+    if mask is not None:
+        stats["mask"] = mask.reindex_like(stats.tavg, method="nearest", tolerance=1e-4)
+    else:
+        stats["mask"] = xr.ones_like(stats.tavg).where(stats.tavg > -100)
 
-    stats["geohash"] = geohash_xr(stats.tavg)
+    stats = stats.where(stats.mask == 1)
+    stats["geohash"] = geohash_xr(stats.mask)
     stats["geohash"].attrs["_FillValue"] = -1
     stats["geohash"].attrs["missing_value"] = -1
 
-    # stats = stats.where(stats.mask == 1)
-
-    # stats2 = stats.copy()
-    # del stats["mask"]
+    del stats["mask"]
 
     df_stats = (
         stats.to_dataframe()
@@ -218,10 +234,8 @@ def main():
     )
     # ignore prec for now
 
-    lookup = {}
+    lookup: Dict[int, ClimateSiteStats] = {}
     for _, row in df_stats.iterrows():
-        # if row["geohash"] != -1:
-        # geohash = coords2geohash_dec(lat=row["lat"], lon=row["lon"])
         lookup[int(row["geohash"])] = ClimateSiteStats(
             id=int(row["geohash"]),
             latitude=row["lat"],
@@ -232,54 +246,75 @@ def main():
             temperature_amplitude=row["tamp"],
         )
 
-    ds_vn["geohash"] = xr.ones_like(ds_vn.tavg.isel(time=0), dtype=np.int64)
-    ds_vn["geohash"] = stats["geohash"]
-    ds_vn = ds_vn.stack(location=("lat", "lon"))
-    ds_vn = ds_vn.swap_dims({"location": "geohash"})
-    ds_vn = ds_vn.set_coords("geohash")
+    ds["geohash"] = stats["geohash"]
+    ds = ds.stack(location=("lon", "lat"))
+    ds = ds.swap_dims({"location": "geohash"})
+    ds = ds.set_coords("geohash")
+    del ds["location"]
 
-    print(ds_vn)
-
-    del ds_vn["location"]
-    ds_vn.to_netcdf("stats.nc")
-
-    ddf = ds_vn.to_dask_dataframe(dim_order=["geohash", "time"])
-    print(ddf.head())
-    print(ddf.dtypes)
-    ddf = ddf[ddf.geohash > -1]
-    print(ddf.head())
-    print("DASK DF:", ddf.shape)
-
-    print("DASK DF 2")
+    ddf = ds.to_dask_dataframe(dim_order=["geohash", "time"])
+    ddf = ddf.loc[ddf.geohash > 0, :]
+    ddf = ddf.set_index("geohash")
+    ddf = ddf.repartition(npartitions=20)
 
     # ignore precip for now???
     ddf = ddf.dropna(subset=["tavg", "tmin", "tmax", "rad", "rh", "wind"], how="all")
-    print("DASK DF 3")
-
-    all_hashes = sorted(list(lookup.keys()))
-    print(len(all_hashes))
-
-    # onehundred_each = all_hashes[0::10] + all_hashes[-1:]
-
-    # ddf = ddf.set_index("geohash", divisions=onehundred_each)
-    # print(ddf.head())
-    # print( "MEMORY TOTAL: ", ddf.memory_usage().sum().compute() / 1024 / 1024 )
-
-    ddf = ddf.set_index("geohash", partition_size="20MB")
-
-    print("DASK DF 4", ddf.npartitions)
-
-    lookup = dask.delayed(lookup)
     partitions = ddf.to_delayed(optimize_graph=True)
+
     formatted = [
-        dask.delayed(writer)(part, i, lookup=lookup)
+        dask.delayed(writer)(part, i, lookup=dask.delayed(lookup), args=dask.delayed(args))
         for i, part in enumerate(partitions)
-    ]
+    ]   
 
-    dask.compute(*formatted)
-
+    processed_geohashs = dask.compute(*formatted)
+    with open(args.outfolder / "ids.txt", "w") as out:
+        for chunk_geohashs in processed_geohashs:
+            out.write(" ".join([f"{ghash}" for ghash in chunk_geohashs]) + "\n")
 
 if __name__ == "__main__":
-    client = Client(processes=True)
-    print(client)
-    main()
+    client = Client(dashboard_address=':1234')
+
+    print(f"If you installed bokeh you can observe progress at {platform.node()}:1234")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("outfolder", nargs='?', type=lambda p: Path(p).absolute(), default=Path.cwd() / "output", help="outpath for climate archive files")
+
+    parser.add_argument(
+        "-b",
+        "--bbox",
+        dest="bbox",
+        default=None,
+        metavar="[X1,Y1,X2,Y2]",
+        help="bounding box",
+    )
+
+    parser.add_argument(
+        "-m",
+        "--mask",
+        dest="mask",
+        default=None,
+        help="netcdf file with mask variable [format: filename.nc:var]",
+    )    
+
+    parser.add_argument(
+        "--dmin",
+        dest="date_min",
+        default=None,
+        type=str,
+        help="minimum date to consider [format: 2000-01-01]",
+    )    
+
+    parser.add_argument(
+        "--dmax",
+        dest="date_max",
+        default=None,
+        type=str,
+        help="maximum date to consider [format: 2001-12-31]",
+    )    
+
+
+    args = parser.parse_args()
+    # create args.outfolder if it does not exist
+    args.outfolder.mkdir(parents=True, exist_ok=True)
+
+    main(args)
